6fdf56835e96f293f4280cb0dcd13a63370cc434
Paul-Elvis <hellotopaul@gmail.com>
2022-12-15 20:45:50 -0500

    first commit

---
+++ src/test/java/ca/tpe/housemanager/HouseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tpe.housemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
